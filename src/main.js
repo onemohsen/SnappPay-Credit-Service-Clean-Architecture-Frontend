b7f5d06d03884e22d1e0493feb5b4ba3f8378f0f
Mohsen Farzadipour <onemohsen@gmail.com>
2022-05-20 18:06:54 +0430

    feat: add vue api query package

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import App from './App.vue'
 import router from './router'
 import './assets/index.css'
 
+
+import axios from 'axios'
+import { Model } from 'vue-api-query'
+
+// inject global axios instance as http client to Model
+Model.$http = axios
+
 const app = createApp(App)
 
 app.use(createPinia())
