1ebe5b075de2d99248e4bfae8ccf404f71648515
Mohsen Farzadipour <onemohsen@gmail.com>
2022-05-20 20:23:05 +0430

    feat: add page route directory package

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
-<script setup>
+<template>
+  <div
+    id="wrapper"
+  >
+    <Header />
+    <ElProgress
+      v-if="loading"
+      :percentage="100"
+      :indeterminate="true"
+      :show-text="false"
+      :duration="2"
+      :stroke-width="2"
+    />
+    <!-- <ElMain
+			id="content"
+			class="flex-fill p-0"
+		> -->
+    <ElContainer v-if="error">
+      {{ error.message }}
+    </ElContainer>
+    <RouterView v-slot="{ Component, route }">
+      <Transition mode="out-in">
+        <div :key="route.meta?.key || route.path">
+          <Suspense>
+            <template #default>
+              <Component
+                :is="Component"
+                :key="$route.fullPath"
+              />
+            </template>
+          </Suspense>
+        </div>
+      </Transition>
+    </RouterView>
+    <!-- </ElMain> -->
+    <Footer />
+  </div>
+</template>
+<script>
 
 </script>
-
-<template>
-    <div>
-        <h1 class="bg-red-500">Hello World</h1>
-        <p>This is my first Vue.js app.</p>
-         <el-button type="primary">Primary</el-button>
-    </div>
-</template>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Pages from 'vite-plugin-pages'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +18,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    Pages(),
   ],
   resolve: {
     alias: {
